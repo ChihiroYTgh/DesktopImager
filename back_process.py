787c640bd386b12aa1c6b604aaf1a92233a685bf
desktop1 <thxchihiro2016@gmail.com>
2023-06-14 01:58:16 +0900

    canvas内にボタンを配置

--- back_process.py
+++
@@ -1,8 +0,0 @@
-import os, tkinter, tkinter.filedialog
-
-def select_file():
-    idir = os.path.expanduser('~/Desktop')
-    filetype = [("PNG","*.png"), ("JPG","*.jpg"), ("すべて","*")]
-    file_path = tkinter.filedialog.askopenfilename(filetypes = filetype, initialdir = idir)
-    return file_path
-    
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter, tkinter.filedialog
-from back_process import select_file
-import math
+import math,os
 
 root = tkinter.Tk()
 root.title("desktoping")  #title
@@ -19,20 +18,26 @@ def create_view(file_path):
         height = math.ceil(origin_img.height*1280/origin_img.width)
         hd_img = origin_img.resize((1280,height))
     preview_img = ImageTk.PhotoImage(hd_img)
-    preview_canvas.create_image(0, 0, image=preview_img, tag='preview_img_tag', anchor=tkinter.NW,)
+    preview_canvas.create_image(642, 362, image=preview_img, tag='preview_img_tag', anchor=tkinter.CENTER)
     return preview_img
+
+def select_file():
+    idir = os.path.expanduser('~/Desktop')
+    filetype = [("PNG","*.png"), ("JPG","*.jpg"), ("すべて","*")]
+    file_path = tkinter.filedialog.askopenfilename(filetypes = filetype, initialdir = idir)
+    return file_path
 
 #画像の移動
 def view_move(dir):
     print(preview_canvas.bbox("preview_img_tag"))
     if dir == 'up':
-        preview_canvas.move("preview_img_tag", 0, -8)
-    elif dir == 'down':
         preview_canvas.move("preview_img_tag", 0, 8)
+    elif dir == 'down':
+        preview_canvas.move("preview_img_tag", 0, -8)
     elif dir == 'left':
-        preview_canvas.move("preview_img_tag", -8, 0)
-    elif dir == 'right':
         preview_canvas.move("preview_img_tag", 8, 0)
+    elif dir == 'right':
+        preview_canvas.move("preview_img_tag", -8, 0)
     print(preview_canvas.bbox("preview_img_tag"))
 
 def file_open():
@@ -41,7 +46,7 @@ def file_close():
     try:
         preview_canvas.delete("preview_img_tag")
     except:
-        preview_canvas.create_text(640,360,text="現在ファイルは指定されていません。")
+        preview_canvas.create_text(640,360,text="現在ファイルは指定されていません。",anchor='center')
 
 
 # menubarの大元（コンテナ）の作成と設置
@@ -55,19 +60,20 @@ menu_file.add_command(label='閉じる', command=file_close)
     
 # btn_file_open = tkinter.Button(root, text='ファイルを選択する', command=file_open)
 # btn_file_open.place(x=1300, y=25)
+
+
+preview_canvas = tkinter.Canvas(root, bg="white", height=720, width=1280)
+preview_canvas.place(x=50, y=50)
+preview_canvas.pack()
+
+
 btn_img_up = tkinter.Button(root, text='↑', command=lambda:view_move('up'))
-btn_img_up.place(x=1350, y=50)
+preview_canvas.create_window(512,2, anchor='nw',window=btn_img_up,width=256)
 btn_img_down = tkinter.Button(root, text='↓', command=lambda:view_move('down'))
-btn_img_down.place(x=1350, y=100)
+preview_canvas.create_window(512,720, anchor='sw',window=btn_img_down,width=256)
 btn_img_left = tkinter.Button(root, text='←', command=lambda:view_move('left'))
-btn_img_left.place(x=1300, y=75)
+preview_canvas.create_window(2,288, anchor='nw',window=btn_img_left,height=144)
 btn_img_right = tkinter.Button(root, text='→', command=lambda:view_move('right'))
-btn_img_right.place(x=1400, y=75)
-
-preview_canvas = tkinter.Canvas(root, bg="white", height=718, width=1278)
-preview_canvas.place(x=0, y=0)
-
-
-
+preview_canvas.create_window(1282,288, anchor='ne',window=btn_img_right,height=144)
 # ウィンドウの描画
 root.mainloop()
