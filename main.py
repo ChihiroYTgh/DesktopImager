50f54b57069af3e159268d6c62617ab2eff4458c
chihiro <5211014@st.hsc.ac.jp>
2023-07-05 11:31:02 +0900

    微修正

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ class Desktop_imagerApp:
         self.desk_sr_w, self.desk_sr_h = self.desk_w // gcd, self.desk_h // gcd
         
         self.preview_canvas = tk.Canvas(self.simple_mode, bg="white", highlightthickness=0)
-        self.btn_file_open = tk.Button(self.preview_canvas, text='ファイルを選択する', command=self.file_open)
+        self.conse_var = tk.BooleanVar()
+        self.btn_file_open = tk.Button(self.preview_canvas, text='ファイルを選択する', command=self.file_open,
+                                       font=("BIZ UDPゴシック",18), )
+        self.box_consecutive = tk.Checkbutton(self.preview_canvas, text="切り抜き後も作業を続ける", command=self.check_conse, variable=self.conse_var,
+                                                font=("BIZ UDPゴシック",24), fg="#333239", activeforeground="#333239")
         self.preview_canvas.create_window(self.desk_w // 2, self.desk_h // 2, anchor=tk.CENTER, tags='select_btn', window=self.btn_file_open)
+        self.preview_canvas.create_window(0, 0, anchor=tk.NW, tags='conse_box',window=self.box_consecutive)
         self.preview_canvas.pack(fill=tk.BOTH, expand=True)
-        
+
         self.origin_img = None
         self.preview_img = None
         self.reset_x = None
@@ -29,6 +34,7 @@ class Desktop_imagerApp:
         self.folder_path = None
         self.pv_x = None
         self.pv_y = None
+
         
         self.create_view()
         
@@ -37,6 +43,7 @@ class Desktop_imagerApp:
     def create_view(self):
         try:
             self.origin_img = Image.open(self.file_path)
+            
             img_w = self.origin_img.width
             img_h = self.origin_img.height
             gcd = math.gcd(img_w, img_h)
@@ -50,11 +57,14 @@ class Desktop_imagerApp:
             self.preview_img = ImageTk.PhotoImage(self.preview_img)
             self.preview_canvas.create_image(self.preview_canvas.winfo_width() // 2, self.preview_canvas.winfo_height() // 2, image=self.preview_img, tag='preview_img_tag', anchor=tk.CENTER)
             self.reset_x, self.reset_y, reset_w, reset_h = self.preview_canvas.bbox('preview_img_tag')
+            self.preview_canvas.itemconfigure('select_btn', state=tk.HIDDEN)
             self.preview_canvas.bind("<Button-1>", self.click_in)
             self.preview_canvas.bind("<Button1-Motion>", self.click_hold)
             self.preview_canvas.bind("<ButtonRelease-1>", self.click_out)
         except:
-            pass
+            return
+        
+
     def click_in(self, event):
         self.pv_x = event.x
         self.pv_y = event.y
@@ -81,7 +91,7 @@ class Desktop_imagerApp:
     
     def select_file(self):
         dir = os.path.expanduser('~/Desktop')
-        filetype = [("すべて", "*"), ("PNG", "*.png"), ("JPG", "*.jpg"), ("JPEG", "*.jpeg")]
+        filetype = [("画像ファイル", "*.png;*.jpg;*.jpeg;*.tif*.tiff;*.bmp")]
         self.file_path = filedialog.askopenfilename(filetypes=filetype, initialdir=dir)
         self.folder_path = os.path.dirname(self.file_path)
     
@@ -89,21 +99,17 @@ class Desktop_imagerApp:
         self.preview_canvas.delete("preview_img_tag")
         self.select_file()
         self.create_view()
-        self.preview_canvas.itemconfigure('select_btn', state=tk.HIDDEN)
         
     
     def file_close(self):
-        try:
-            self.preview_canvas.delete("preview_img_tag")
-            self.preview_canvas.itemconfigure('select_btn', state=tk.NORMAL)
-            self.preview_canvas.unbind("<Button-1>", self.click_in)
-            self.preview_canvas.unbind("<Button1-Motion>", self.click_hold)
-            self.preview_canvas.unbind("<ButtonRelease-1>", self.click_out)
-        except:
-            pass
-    
+        self.preview_canvas.delete("preview_img_tag")
+        self.preview_canvas.itemconfigure('select_btn', state=tk.NORMAL)
+        self.preview_canvas.unbind("<Button-1>", self.click_in)
+        self.preview_canvas.unbind("<Button1-Motion>", self.click_hold)
+        self.preview_canvas.unbind("<ButtonRelease-1>", self.click_out)
+
     def preview_img_save(self):
-        if self.preview_img is None:
+        if self.preview_img is None:    
             pass
         else:
             self.simple_mode.attributes("-alpha", 1.0)
@@ -112,23 +118,19 @@ class Desktop_imagerApp:
             timestamp = now.strftime("%Y%m%d_%H%M%S")
             save_path = file_name + "_" + timestamp + ".png"
             ImageGrab.grab(bbox=(0, 0, self.desk_w, self.desk_h)).save(save_path)
-            self.file_close()
             try:
-                subprocess.run(["start", save_path], shell=True)
+                subprocess.run(["start", save_path], shell=True)    #windowsの場合
             except:
                 try:
-                    subprocess.run(["open", save_path], shell=True)
+                    subprocess.run(["open", save_path], shell=True) #MacOSの場合
                 except:
                     pass
-            self.simple_mode.destroy()
     
     def close_simple_mode(self, event):
         self.simple_mode.destroy()
 
-    def change_mode(self):
-        self.file_close()
-        self.simple_mode.destroy()
-        self.batch_mode()
+    def check_conse(self):
+        print(self.conse_var.get())
 
     def batch_mode(self):
         self.batch_mode = tk.Tk()
@@ -136,14 +138,14 @@ class Desktop_imagerApp:
         self.batch_mode.title("Desktopping")
         self.batch_mode.mainloop()
 
-    def press_keys(self, event):
+    def press_keys(self, event):    # 4はCtrlキー/8はNumLockキーのステート値
         if event.keysym == "Escape":
             self.simple_mode.quit()
-        elif event.keysym == "o" and (event.state == 4 or 12):  # 4はCtrlキーのステート値
+        elif event.keysym == "o" and (event.state == 4 or 12):  #プレビューに表示する画像を選択
             self.file_open()
-        elif event.keysym == "w" and (event.state == 4 or 12):  # 4はCtrlキーのステート値
+        elif event.keysym == "w" and (event.state == 4 or 12):  #プレビューから画像を削除
             self.file_close()
-        elif event.keysym == "s" and (event.state == 4 or 12):  # +8はNumLockキーのステート値
+        elif event.keysym == "s" and (event.state == 4 or 12):  #プレビューの画像をキャンバスサイズで保存
             self.preview_img_save()
         elif event.keysym == "r" and (event.state == 4 or 12):  #画像の表示位置を初期化
             try:
