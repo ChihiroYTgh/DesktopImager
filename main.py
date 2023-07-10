d7749121c9198df68ee27dfd7b7e27db8b43d42f
chihiro <5211014@st.hsc.ac.jp>
2023-07-10 15:59:42 +0900

    カラーリングの変更

--- main.py
+++ main.py
@@ -16,12 +16,19 @@ class Desktop_imagerApp:
         gcd = math.gcd(self.desk_w, self.desk_h)
         self.desk_sr_w, self.desk_sr_h = self.desk_w // gcd, self.desk_h // gcd
         
-        self.preview_canvas = tk.Canvas(self.simple_mode, bg="white", highlightthickness=0)
+        self.preview_canvas = tk.Canvas(self.simple_mode, bg="#1f2447", highlightthickness=0)
         self.conse_var = tk.BooleanVar()
         self.btn_file_open = tk.Button(self.preview_canvas, text='ファイルを選択する', command=self.file_open,
-                                       font=("BIZ UDPゴシック",18), )
-        self.box_consecutive = tk.Checkbutton(self.preview_canvas, text="切り抜き後も作業を続ける", command=self.check_conse, variable=self.conse_var,
-                                                font=("BIZ UDPゴシック",24), fg="#333239", activeforeground="#333239")
+                                       font=("BIZ UDPゴシック",18),
+                                       bg="#f3f4f8",
+                                       fg="#1f2447")
+        self.box_consecutive = tk.Checkbutton(self.preview_canvas, text="切り抜き後も作業を続ける", variable=self.conse_var,
+                                                selectcolor="#1f2447",
+                                                font=("BIZ UDPゴシック",24),
+                                                bg="#1f2447",
+                                                activebackground="#1f2447",
+                                                fg="#f3f4f8",
+                                                activeforeground="#f3f4f8")
         self.preview_canvas.create_window(self.desk_w // 2, self.desk_h // 2, anchor=tk.CENTER, tags='select_btn', window=self.btn_file_open)
         self.preview_canvas.create_window(0, 0, anchor=tk.NW, tags='conse_box',window=self.box_consecutive)
         self.preview_canvas.pack(fill=tk.BOTH, expand=True)
@@ -34,9 +41,6 @@ class Desktop_imagerApp:
         self.folder_path = None
         self.pv_x = None
         self.pv_y = None
-
-        
-        self.create_view()
         
         self.simple_mode.mainloop()
     
@@ -58,9 +62,10 @@ class Desktop_imagerApp:
             self.preview_canvas.create_image(self.preview_canvas.winfo_width() // 2, self.preview_canvas.winfo_height() // 2, image=self.preview_img, tag='preview_img_tag', anchor=tk.CENTER)
             self.reset_x, self.reset_y, reset_w, reset_h = self.preview_canvas.bbox('preview_img_tag')
             self.preview_canvas.itemconfigure('select_btn', state=tk.HIDDEN)
-            self.preview_canvas.bind("<Button-1>", self.click_in)
-            self.preview_canvas.bind("<Button1-Motion>", self.click_hold)
-            self.preview_canvas.bind("<ButtonRelease-1>", self.click_out)
+            self.preview_canvas.itemconfigure('conse_box', state=tk.HIDDEN)
+            self.id_click = self.preview_canvas.bind("<Button-1>", self.click_in)
+            self.id_hold = self.preview_canvas.bind("<Button1-Motion>", self.click_hold)
+            self.id_release = self.preview_canvas.bind("<ButtonRelease-1>", self.click_out)
         except:
             return
         
@@ -90,7 +95,10 @@ class Desktop_imagerApp:
             self.preview_canvas.move('preview_img_tag', 0, -yNW)
     
     def select_file(self):
-        dir = os.path.expanduser('~/Desktop')
+        if self.folder_path == None:
+            dir = os.path.expanduser('~/Desktop')
+        else:
+            dir = self.folder_path
         filetype = [("画像ファイル", "*.png;*.jpg;*.jpeg;*.tif*.tiff;*.bmp")]
         self.file_path = filedialog.askopenfilename(filetypes=filetype, initialdir=dir)
         self.folder_path = os.path.dirname(self.file_path)
@@ -104,9 +112,10 @@ class Desktop_imagerApp:
     def file_close(self):
         self.preview_canvas.delete("preview_img_tag")
         self.preview_canvas.itemconfigure('select_btn', state=tk.NORMAL)
-        self.preview_canvas.unbind("<Button-1>", self.click_in)
-        self.preview_canvas.unbind("<Button1-Motion>", self.click_hold)
-        self.preview_canvas.unbind("<ButtonRelease-1>", self.click_out)
+        self.preview_canvas.itemconfigure('conse_box', state=tk.NORMAL)
+        self.preview_canvas.unbind("<Button-1>", self.id_click)
+        self.preview_canvas.unbind("<Button1-Motion>", self.id_hold)
+        self.preview_canvas.unbind("<ButtonRelease-1>", self.id_release)
 
     def preview_img_save(self):
         if self.preview_img is None:    
@@ -118,6 +127,13 @@ class Desktop_imagerApp:
             timestamp = now.strftime("%Y%m%d_%H%M%S")
             save_path = file_name + "_" + timestamp + ".png"
             ImageGrab.grab(bbox=(0, 0, self.desk_w, self.desk_h)).save(save_path)
+            
+        #事後処理(GUI)
+        if self.conse_var.get() == True:
+            self.simple_mode.attributes("-alpha", 0.8)
+            self.file_close()
+            self.file_open()
+        else:
             try:
                 subprocess.run(["start", save_path], shell=True)    #windowsの場合
             except:
@@ -125,12 +141,7 @@ class Desktop_imagerApp:
                     subprocess.run(["open", save_path], shell=True) #MacOSの場合
                 except:
                     pass
-    
-    def close_simple_mode(self, event):
-        self.simple_mode.destroy()
-
-    def check_conse(self):
-        print(self.conse_var.get())
+            self.simple_mode.destroy()
 
     def batch_mode(self):
         self.batch_mode = tk.Tk()
